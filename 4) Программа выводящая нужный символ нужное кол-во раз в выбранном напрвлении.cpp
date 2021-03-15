fefeacd5e91e1810d24eaa82a38f2f0778e500e0
HumsterZOO <80671429+HumsterZOO@users.noreply.github.com>
2021-03-15 23:54:27 +0300

    Add files via upload

---
+++ 4) Программа выводящая нужный символ нужное кол-во раз в выбранном напрвлении.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	int index=0,number, direction;
+	char  tipe;
+	cout << "Введите количевство нужных символов :" << endl;
+	cin >> number;
+	cout << "Введите нужный символ :" << endl;
+	cin >> tipe;
+	cout << "\tВыберите направление отоброжения\n1 - Горизонтально\n2 - Вертикально" << endl;
+	cin >> direction;
+	while (index < number)
+	{
+		if (direction == 1)
+		{
+			cout << tipe;
+			++index;
+		}
+		if (direction == 2)
+		{
+			cout << tipe << endl;
+			++index;
+		}
+	}
+
+}
