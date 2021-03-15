6f0d475577e5ce4404c224e547d68c6084a484b4
HumsterZOO <80671429+HumsterZOO@users.noreply.github.com>
2021-03-15 12:32:39 +0300

    Add files via upload

---
+++ 2) Проверка числа на чётность.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+		int a;
+		cout << "Введите число для проверки на чётность" << endl;
+	cin >> a;
+	if (a % 2 == 0)
+	{
+		cout << "Ваше число чётное" << endl;
+	}
+	else
+	{
+		cout << "Ваше число не чётное" << endl;
+	}
+}
