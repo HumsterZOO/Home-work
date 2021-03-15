cc1bfe117116c1a48ce6511dcb8805e6280aa4b0
HumsterZOO <80671429+HumsterZOO@users.noreply.github.com>
2021-03-15 10:16:50 +0300

    Add files via upload

---
+++ Программа для расчёта суммы, произведения, среднего арифметического.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, b, c;
+	cout << "Программа для расчёта"<< endl << "Введите первое число: "<<endl;
+	cin >> a;
+	cout << "Введите второе число: " << endl;
+	cin >> b;
+	cout << "Введите третье число: " << endl;
+	cin >> c;
+		cout << "Сумма равна: " << a + b + c << endl << "Произведение равно: " << a * b * c << endl << "Среднее арифметическое: "<< (double)(a + b + c) / 3 << endl;
+
+
+}
+
