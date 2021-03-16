e1d00a9c922d1ec1ad35455ed0777308227b5d5e
HumsterZOO <80671429+HumsterZOO@users.noreply.github.com>
2021-03-17 01:30:45 +0300

    Add files via upload

---
+++ 6)Вывод прямоугольника из звёздочек заданой ширины и высоты.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int wigth, height;
+	cout << "Введите ширину прямоугольника." << endl;
+	cin >> wigth;
+	cout << "Введите высоту прямоугольника." << endl;
+	cin >> height;
+	for (int a = 0;a < height;a++)
+	{
+		for (int b = 0;b < wigth;b++)
+		{
+			cout << "*";
+		}
+		cout<< endl;
+	}
+}
