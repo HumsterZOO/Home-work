0e0c3922a6f0eaa074f4a60a0daea84af37a04cf
HumsterZOO <80671429+HumsterZOO@users.noreply.github.com>
2021-03-17 01:31:00 +0300

    Add files via upload

---
+++ 7)Запись случайных не повторяющихся (уникальных) чисел в масив..cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<ctime>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+		srand(time(NULL));
+		const int SIZE = 10;
+		int arr[SIZE];
+		for (int i = 0;i < SIZE;i++)
+		{
+			arr[i] = rand() % 20;
+			for (int b = 0;b < i;b++)
+			{
+				if(arr[i]==arr[b])
+				{
+				arr[i]= rand() % 20;
+				b = 0;
+				}
+			}
+		}
+		for(int i =0;i<SIZE;i++)
+		{
+			cout << arr[i] << endl;
+		}
+
+
+
+
+}
